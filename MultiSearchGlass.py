d4bb03378a0172270829ef902e74598a4d677719
OAkimasa <80835540+OAkimasa@users.noreply.github.com>
2021-12-11 16:52:51 +0900

    45re完了
    一区切りついた

--- MultiSearchGlass.py
+++ MultiSearchGlass.py
@@ -15,7 +15,7 @@ from setting.MacroGlassMatrix import returnFocus
 from setting.MacroGlassMatrix_Lens12 import returnFocus_Lens12
 from setting.MacroGlassMatrix_Lens45 import returnFocus_Lens45
 from setting.MacroGlassMatrix_Lens12re import returnFocus_Lens12re
-#from setting.MacroGlassMatrix_Lens45re import returnFocus_Lens45re
+from setting.MacroGlassMatrix_Lens45re import returnFocus_Lens45re
 
 from setting.Out_GlassList_4core import trainParam
 from setting.Out_GlassList_4core2 import trainParam2
@@ -95,7 +95,7 @@ def infMakeFocusList_Lens45(args):
 
 
 
-# 作成中
+
 # レンズ12re、探索
 def infMakeFocusList_Lens12re(args):
     results = []
@@ -107,7 +107,7 @@ def infMakeFocusList_Lens12re(args):
             result = (focus, i)
             results.append(result)
     return results
-'''
+
 # レンズ45re、探索
 def infMakeFocusList_Lens45re(args):
     results = []
@@ -119,8 +119,7 @@ def infMakeFocusList_Lens45re(args):
             result = (focus, i)
             results.append(result)
     return results
-'''
-# 作成中
+
 
 
 
@@ -160,7 +159,7 @@ def out_csvFile(args):
         if i != []:
             results.append(i)
 
-    file = open('Out_GlassList_after_inf12re.csv', 'w', newline='')
+    file = open('Out_GlassList_after_inf45re.csv', 'w', newline='')
     writer = csv.writer(file)
     writer.writerows(results)
     file.close()
@@ -174,7 +173,7 @@ if __name__ == "__main__":
     ParamsMatrix = searchParam_GlassData_Lens12and45()  # 総当たりか既存のリストか
     print('-------------calculating-------------')
     p = Pool(processes=4)
-    results = p.map(func=infMakeFocusList_Lens12re, iterable=ParamsMatrix)
+    results = p.map(func=infMakeFocusList_Lens45re, iterable=ParamsMatrix)
     out_csvFile(results)
     #print('result =', results)
 
